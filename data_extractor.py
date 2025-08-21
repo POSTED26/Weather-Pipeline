c13ba26bf6273be4263cdcad0b785fb3e3cf7e2c
tilbo26 <tilbo2@gmail.com>
2025-08-21 13:45:45 -0700

    Added some new columns and conctat the citys into one df

--- data_extractor.py
+++ data_extractor.py
@@ -14,8 +14,9 @@ def get_data(url_type, endpoint, params=None):
     Get data from weather api
 
     Args:
-        endpoint (str): endpoint you want to pull data from (meeting, session, driver, etc.)
-        params (dict): optional, drill down more specifics for getting data
+        url_type (str): api url
+        endpoint (str): endpoint to use with the api url
+        params (dict): params to pass with api call
 
     Rreturn:
         pd.DataFrame: returns a data frame for easy clean and manipulation 
@@ -26,5 +27,60 @@ def get_data(url_type, endpoint, params=None):
     url = f"{url_type}{endpoint}"
     full_url = requests.Request('GET', url, params=params).prepare().url
     response = requests.get(full_url)
+    if endpoint == 'weather':
+        return weather_data(response.json())
+    
     response.raise_for_status()
     return pd.DataFrame(response.json())
+
+
+# Clean up weather json to make work with pandas dataFrame
+def weather_data(json):
+    df = pd.json_normalize(json,
+                            'weather',
+                            [['main', 'temp'],
+                            ['main', 'feels_like'],
+                            ['main', 'temp_min'],
+                            ['main', 'temp_max'],
+                            ['main', 'pressure'],
+                            ['main', 'humidity'],
+                            'visibility', 
+                            ['wind', 'speed'],
+                            ['wind', 'deg'],
+                            'dt',
+                            'timezone',
+                            'name',
+                            ['coord','lat'],
+                            ['coord','lon']])
+    df = data_clean(df)
+    #print(df.columns)
+
+    
+    #print(df.dtypes)
+    return df
+
+def data_clean(df):
+    df = df.rename(columns={'id': 'weather_id',
+               'main':'weather',
+               'description':'weather_description',
+               'main.temp':'temperature',
+               'main.feels_like':'temperature_feels_like',
+               'main.temp_min':'minimum_temperature',
+               'main.temp_max':'maximum_temperature',
+               'main.pressure':'pressure',
+               'main.humidity':'humidity',
+               'wind.speed':'wind_speed',
+               'wind.deg':'wind_direction',
+               'dt':'date_time_unix_utc',
+               'timezone':'timezone_offset',
+               'name': 'location',
+               'coord.lat': 'lat',
+               'coord.lon':'lon'})
+    df = df.convert_dtypes().copy()
+    df['date_time_unix_local'] = df['date_time_unix_utc'] + df['timezone_offset']
+    df['temperature_c'] = df['temperature'] - 273.15
+    df['temperature_feels_like_c'] = df['temperature_feels_like'] - 273.15
+    df['minimum_temperature_c'] = df['minimum_temperature'] - 273.15
+    df['maximum_temperature_c'] = df['maximum_temperature'] - 273.15
+
+    return df
--- main.py
+++ main.py
@@ -21,11 +21,25 @@ GEO_URL = os.getenv('GEODATA_API_URL')
 WEATHER_URL = os.getenv('WEATHER_API_URL')
 
 def main():
-   df = data_extractor.get_data(GEO_URL, 'direct', {'q': 'Seattle', 'appid': API_KEY})
-   print(df)
+    city_dict = {'Seattle': (None, None), 'Albuquerque': (None, None)}
+    weather_df = pd.DataFrame()
+    #print('before', city_dict)
+    for city in city_dict.keys():
+        geo_df = data_extractor.get_data(GEO_URL, 'direct', {'q': city, 'appid': API_KEY})
+        #print(geo_df)
+        lat = geo_df.loc[0, 'lat']
+        lon = geo_df.loc[0, 'lon']
+        city_dict[city] = (lat, lon) 
+        #print(city_dict)
+   
 
-   #TODO: save lat and long, idealy I will have a dict of city: (lat, long) so I can easy pull weather data using this 
-
+        #TODO: save lat and long, idealy I will have a dict of city: (lat, long) so I can easy pull weather data using this 
+        temp_weather_df = data_extractor.get_data(WEATHER_URL, 'weather', {'lat': city_dict[city][0], 'lon': city_dict[city][1], 'appid': API_KEY})
+        #weather_df = pd.merge(temp_weather_df, geo_df, how='left', on=['lat', 'lon'])
+        #print(weather_df)
+        weather_df = pd.concat([weather_df, temp_weather_df], ignore_index=True)
+        
+    print(weather_df[['lat', 'lon', 'location', 'temperature_c']])
 
 
 
